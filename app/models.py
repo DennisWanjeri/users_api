f3eeccea6a32fe7cf972aa57fc84dffaa801aba6
Dennis Wanjeri <77827277+DennisWanjeri@users.noreply.github.com>
2022-06-12 09:55:46 +0200

    restructuring with fastapi

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from .database import Base
+
+
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    body = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False, server_default=text('now()'))
