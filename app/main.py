6ee28125b4a23ddd79e9027796c0bad4f9444a3e
Dennis Wanjeri <77827277+DennisWanjeri@users.noreply.github.com>
2022-06-14 12:24:13 +0200

    restructuring the code

--- app/main.py
+++ app/main.py
@@ -1,21 +1,26 @@
-from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
-import requests
-from .database import engine
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
-from .schemas import PostModel, UpdatedPost
-from sqlalchemy.orm import Session
-from . database import get_db
-from .routers import posts, users
-models.Base.metadata.create_all(bind=engine)
+from .database import engine
+from .routers import users, posts
+from .config import settings
 
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-posts_url = "https://jsonplaceholder.typicode.com/posts"
-users_url = "https://jsonplaceholder.typicode.com/users"
+origins = ["*"]
 
-app.include_router(posts.router)
-app.include_router(users.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# app.include_router(posts.router)
+# app.include_router(users.router)
 
 
 @app.get("/")
